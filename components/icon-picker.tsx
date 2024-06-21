a6650d09c75256b2cf3d593caf3bc584d2d4afea
devisv505 <devisv505@gmail.com>
2024-06-21 17:25:56 +0200

    netter emoji panel

--- components/icon-picker.tsx
+++ components/icon-picker.tsx
@@ -4,14 +4,18 @@ import React from "react";
 import {useTheme} from "next-themes";
 import EmojiPicker, {Theme} from "emoji-picker-react";
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import {Tabs, TabsContent, TabsList, TabsTrigger} from "@/components/ui/tabs";
+import {Button} from "@/components/ui/button";
+import {XIcon} from "lucide-react";
 
 interface IconPickerProps {
     onChange: (icon: string) => void;
+    onDelete: () => void;
     children: React.ReactNode;
     asChild?: boolean;
 }
 
-const IconPicker = ({onChange, children, asChild}: IconPickerProps) => {
+const IconPicker = ({onChange, onDelete, children, asChild}: IconPickerProps) => {
     const {resolvedTheme} = useTheme();
     const currentTheme = (resolvedTheme || "light") as keyof typeof themMap;
     const themMap = {
@@ -26,16 +30,28 @@ const IconPicker = ({onChange, children, asChild}: IconPickerProps) => {
                 {children}
             </PopoverTrigger>
             <PopoverContent className="p-0 w-full">
-                test
 
-                <div className="emoji-picker-wrapper rounded-none border-none shadow-none">
-                    <EmojiPicker
-                        height={350}
-                        theme={theme}
-                        onEmojiClick={(data) => onChange(data.emoji)}
-                        className="rounded-none border-none shadow-none"
-                    />
-                </div>
+                <Tabs defaultValue="account" className="w-[400px]">
+                    <TabsList className="flex justify-between">
+                        <TabsTrigger value="empjis">Emojis</TabsTrigger>
+                        <Button
+                            variant="ghost"
+                            onClick={onDelete}
+                        >
+                            <XIcon className="w-4 h-4 mr-1" />
+                            Remove
+                        </Button>
+                        {/*<TabsTrigger value="password">Password</TabsTrigger>*/}
+                    </TabsList>
+                    <TabsContent value="empjis" className="flex items-center justify-center p-0 m-0">
+                        <EmojiPicker
+                            height={350}
+                            theme={theme}
+                            onEmojiClick={(data) => onChange(data.emoji)}
+                        />
+                    </TabsContent>
+                    {/*<TabsContent value="password">Change your password here.</TabsContent>*/}
+                </Tabs>
             </PopoverContent>
         </Popover>
     );
--- components/toolbar.tsx
+++ components/toolbar.tsx
@@ -9,6 +9,8 @@ import {useMutation} from "convex/react";
 import {api} from "@/convex/_generated/api";
 import TextareaAutosize from "react-textarea-autosize";
 import {useCoverImage} from "@/hooks/use-cover-image";
+import {cn} from "@/lib/utils";
+import {useMediaQuery} from "usehooks-ts";
 
 interface Toolbar {
     initialData: Doc<"documents">;
@@ -19,6 +21,7 @@ const Toolbar = ({initialData, preview}: Toolbar) => {
     const inputRef = useRef<ElementRef<"textarea">>(null);
     const [isEditing, setIsEditing] = useState(false);
     const [value, setValue] = useState(initialData.title);
+    const isMobile = useMediaQuery("(max-width: 768px)");
 
     const update = useMutation(api.documents.update);
     const removeIcon = useMutation(api.documents.removeIcon);
@@ -72,20 +75,13 @@ const Toolbar = ({initialData, preview}: Toolbar) => {
                 <div className="flex items-center gap-x-2 group/icon pt-6">
                     <IconPicker
                         onChange={onIconSelect}
+                        onDelete={onRemoveIcon}
                         asChild
                     >
                         <p className="text-6xl hover:opacity-65 transition">
                             {initialData.icon}
                         </p>
                     </IconPicker>
-                    <Button
-                        onClick={onRemoveIcon}
-                        className="rounded-full opacity-0 group-hover/icon:opacity-100 transition text-muted-foreground text-xs"
-                        variant="outline"
-                        size="icon"
-                    >
-                        <XIcon className="h-4 w-4"/>
-                    </Button>
                 </div>
             )}
             {!!initialData.icon && preview && (
@@ -93,9 +89,12 @@ const Toolbar = ({initialData, preview}: Toolbar) => {
                     {initialData.icon}
                 </p>
             )}
-            <div className="opacity-0 group-hover:opacity-100 flex items-center gap-x-1 py-4">
+            <div className={cn(
+                "flex items-center gap-x-1 py-4",
+                !isMobile && "opacity-0 group-hover:opacity-100"
+            )}>
                 {!initialData.icon && !preview && (
-                    <IconPicker onChange={onIconSelect} asChild>
+                    <IconPicker onChange={onIconSelect} onDelete={onRemoveIcon} asChild>
                         <Button
                             className="text-muted-foreground text-xs"
                             variant="outline"
