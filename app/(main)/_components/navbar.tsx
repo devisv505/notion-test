3e7e8fc8888d755ae95b5bd194453db26a7972ef
devisv505 <devisv505@gmail.com>
2024-06-22 21:47:41 +0200

    publishing

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -4,6 +4,7 @@ import {api} from "@/convex/_generated/api";
 import {Id} from "@/convex/_generated/dataModel";
 import {MenuIcon} from "lucide-react";
 import Title from "@/app/(main)/_components/title";
+import {Publish} from "@/app/(main)/_components/publish";
 
 interface NavbarProps {
     isCollapsed: boolean;
@@ -40,8 +41,11 @@ const Navbar = ({isCollapsed, onResetWidth}: NavbarProps) => {
                         className="h-6 w-6 text-muted-foreground"
                     />
                 )}
-                <div>
+                <div className="flex items-center justify-between w-full">
                     <Title initialData={document}/>
+                    <div className="flex items-center gap-x-2">
+                        <Publish initialData = {document} />
+                    </div>
                 </div>
             </nav>
         </>
